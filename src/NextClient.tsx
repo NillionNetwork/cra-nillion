ad81eecb93057aab2bfd15158ccc69c4e13739f9
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-07-31 11:37:13 -0400

    feat: scaffold for `@nillion/client-*` migration

---
+++ src/NextClient.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+export const NextClient = () => {
+  return <h1>Hi!</h1>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,11 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme';
 import BlindInferencePage from './BlindInferencePage';
+import { NillionClientProvider } from '@nillion/client-react-hooks';
+import { NamedNetwork } from '@nillion/client-core';
+import { createSignerFromKey } from '@nillion/client-payments';
+import { NillionClient } from '@nillion/client-vms';
+import { NextClient } from './NextClient';
 
 const router = createBrowserRouter([
   {
@@ -28,17 +33,38 @@ const router = createBrowserRouter([
         path: '/',
         element: <OperationsPage />,
       },
+      {
+        path: '/next',
+        element: <NextClient />,
+      },
     ],
   },
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
+
+const client = NillionClient.create({
+  network: NamedNetwork.enum.Devnet,
+  userSeed: 'thm',
+  nodeSeed: 'thm',
+
+  overrides: async () => {
+    const signer = await createSignerFromKey(
+      '9a975f567428d054f2bf3092812e6c42f901ce07d9711bc77ee2cd81101f42c5',
+    );
+    return {
+      endpoint: 'http://localhost:8080/nilchain',
+    };
+  },
+});
 
 root.render(
   <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <RouterProvider router={router} />
-  </ThemeProvider>
+    <NillionClientProvider client={client}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </NillionClientProvider>
+  </ThemeProvider>,
 );
